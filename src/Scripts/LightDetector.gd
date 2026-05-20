336938c90a96444b68e28a0028b83320df001aea
Yni-Viar <yniviar@outlook.com>
2026-05-20 18:58:39 +0300

    v8.0.0-beta
    - Fix light detector working when there is no light

--- src/Scripts/LightDetector.gd
+++ src/Scripts/LightDetector.gd
@@ -6,7 +6,7 @@ var time_to_update: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if Settings.setting_res.reflection_probe:
+	if Settings.setting_res.reflection_probe || Settings.setting_res.lighting == SettingsResource.Lighting.NONE:
 		$LightDetector.current = false
 		set_process(false)
 	else:
